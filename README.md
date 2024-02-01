a4bb8e63e5d7563835c92240e31c969d6cb3fe0a
Naitik Lodha <69336744+naitiklodha@users.noreply.github.com>
2024-02-01 21:44:25 +0530

    Boilerplate code (#1)
    Commit #1: by @naitiklodha

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 Project Mirage is a unique endeavour where talented developers from GDSC MPSTME work together to create a web application without direct communication, fostering an environment of uncertainty, surprise, and innovation.
 
 As the project progresses, the mirage will lift and the big picture will come into focus. What creative solutions will emerge from this mystery development process? What ingenious features will our developers dream up while flying solo? The suspense is exhilarating!
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.py
@@ -0,0 +1,28 @@
+# Import necessary modules and functions from Flask
+from flask import render_template, Flask, Response
+
+# Import the generate_random function from the utils module
+from utils.generate import generate_random
+
+# Create a Flask web application instance
+app = Flask(__name__)
+
+# Define a route for the root URL ('/')
+@app.route('/')
+def hello_world():
+    # Render the 'index.html' template 
+    return render_template('index.html')
+
+# Define a route for the '/generate-random' URL
+@app.route('/generate-random')
+def generate():
+    # Call the generate_random function from the utils module
+    number = generate_random()
+    
+    # Return the generated number as a plain text response
+    return Response(str(number), content_type='text/plain')
+
+# Run the Flask application if this script is executed directly
+if __name__ == '__main__':
+    # Enabling debugging mode for easier development
+    app.run(debug=True)
