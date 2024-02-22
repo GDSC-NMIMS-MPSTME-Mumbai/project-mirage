e92a106e7c39ce5147cd97f279793b0598bd3095
omanaokar <omanaokar@gmail.com>
2024-02-22 16:52:16 +0530

    Added user authentication routes and functions using firebase

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 # Import necessary modules and functions from Flask
-from flask import render_template, Flask, Response
+from flask import render_template, Flask, Response, session, request, redirect
+import pyrebase
 
 # Import the generate_random function from the utils module
 from utils.generate import generate_random
 
 # Create a Flask web application instance
 app = Flask(__name__)
+
+# Initializing firebase app and user authentication
+config = {
+    "apiKey": "AIzaSyDlq0E6KA4cnxqweE4EcnyRFtCCFqUNc4I",
+    "authDomain": "ai-germ-site.firebaseapp.com",
+    "projectId": "ai-germ-site",
+    "storageBucket": "ai-germ-site.appspot.com",
+    "messagingSenderId": "429718411683",
+    "appId": "1:429718411683:web:d80cfd48a3afe57f32e216",
+    "measurementId": "G-7R47RXR1VS",
+    "databaseURL" : ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+app.secret_key = 'secret'
 
 # Define a route for the root URL ('/')
 @app.route('/')
@@ -29,6 +47,43 @@ def generate():
 @app.route('/filter')
 def filter():
     return render_template('filter.html')
+
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if 'user' in session:
+        return redirect('/pixel-art')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        pwd = request.form.get('password')
+        try:
+            session['user'] = email
+            user = auth.sign_in_with_email_and_password(email,pwd)
+            return render_template('pixel-art.html', email=email)
+        except:
+            return 'Failed to login'
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST','GET'])
+def register():
+    if 'user' in session:
+        return redirect('/pixel-art')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        pwd = request.form.get('password')
+        try:
+            session['user'] = email
+            user = auth.create_user_with_email_and_password(email,pwd)
+            return render_template('pixel-art.html')
+        except:
+            return 'Failed to register'
+    
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/pixel-art')
 
 # Run the Flask application if this script is executed directly
 if __name__ == '__main__':
