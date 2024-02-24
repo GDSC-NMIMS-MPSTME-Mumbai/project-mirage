7aab9396350307675793bef115af0d79d4887ef5
Manan Gandhi <ardumanan@gmail.com>
2024-02-24 17:11:46 +0530

    added rickroll, random color picker, thala yt video

--- static/js/pixel-art.js
+++ static/js/pixel-art.js
@@ -1,148 +1,154 @@
 // pixel-art.js
-const board = document.getElementById("drawingBoard");
-const gridSizeSlider = document.getElementById("gridSizeSlider");
-let currentColor = "black";
-let isMouseDown = false;
-let gridSize = 16; // Default grid size
-let download = document.getElementById("downloadBtn");
-download.addEventListener("click", downloadImage);
+const board = document.getElementById('drawingBoard')
+const gridSizeSlider = document.getElementById('gridSizeSlider')
+let currentColor = 'black'
+let isMouseDown = false
+let gridSize = 16 // Default grid size
+let download = document.getElementById('downloadBtn')
+download.addEventListener('click', downloadImage)
 
 // Function to create the grid of pixels
-function createPixels(size) {
-  board.innerHTML = ""; // Clear the board before redrawing
-  board.style.gridTemplateColumns = `repeat(${size},  1fr)`;
-  board.style.gridTemplateRows = `repeat(${size},  1fr)`;
+function createPixels (size) {
+  board.innerHTML = '' // Clear the board before redrawing
+  board.style.gridTemplateColumns = `repeat(${size},  1fr)`
+  board.style.gridTemplateRows = `repeat(${size},  1fr)`
 
-  const cellSize = board.clientWidth / size; // Calculate the size of each cell
+  const cellSize = board.clientWidth / size // Calculate the size of each cell
 
   for (let i = 0; i < size * size; i++) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    pixel.classList.add("cell-border");
+    const pixel = document.createElement('div')
+    pixel.classList.add('pixel')
+    pixel.classList.add('cell-border')
 
-    pixel.setAttribute("draggable", "false");
-    pixel.addEventListener("dragstart", (event) => {
-      event.preventDefault();
-    });
-    pixel.style.width = `${cellSize}px`;
-    pixel.style.height = `${cellSize}px`;
-    pixel.addEventListener("mousedown", startDrawing);
-    pixel.addEventListener("mouseenter", draw);
-    pixel.addEventListener("mouseup", stopDrawing);
-    board.appendChild(pixel);
+    pixel.setAttribute('draggable', 'false')
+    pixel.addEventListener('dragstart', event => {
+      event.preventDefault()
+    })
+    pixel.style.width = `${cellSize}px`
+    pixel.style.height = `${cellSize}px`
+    pixel.addEventListener('mousedown', startDrawing)
+    pixel.addEventListener('mouseenter', draw)
+    pixel.addEventListener('mouseup', stopDrawing)
+    board.appendChild(pixel)
   }
 }
 
-function startDrawing(event) {
-  isMouseDown = true;
-  draw(event);
+function startDrawing (event) {
+  isMouseDown = true
+  draw(event)
 }
 
-function draw(event) {
-  if (!isMouseDown) return;
+function draw (event) {
+  if (!isMouseDown) return
   if (isEraserMode) {
-    event.target.style.backgroundColor = "";
+    event.target.style.backgroundColor = ''
   } else if (isRandomMode) {
-    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    event.target.style.backgroundColor = randomColor;
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    event.target.style.backgroundColor = randomColor
   } else if (isColorSelectionMode) {
-    event.target.style.backgroundColor = currentColor;
+    event.target.style.backgroundColor = currentColor
   }
 }
 
-function stopDrawing() {
-  isMouseDown = false;
+function stopDrawing () {
+  isMouseDown = false
 }
 
-gridSizeSlider.addEventListener("input", (event) => {
-  gridSize = parseInt(event.target.value, 10);
-  createPixels(gridSize);
+gridSizeSlider.addEventListener('input', event => {
+  gridSize = parseInt(event.target.value, 10)
+  createPixels(gridSize)
 
   // Display the grid size next to the slider
-  const gridSizeDisplay = document.getElementById("gridSizeDisplay");
+  const gridSizeDisplay = document.getElementById('gridSizeDisplay')
   if (gridSizeDisplay) {
-    gridSizeDisplay.textContent = `${gridSize}x${gridSize}`;
+    gridSizeDisplay.textContent = `${gridSize}x${gridSize}`
   }
-});
-function downloadImage() {
-  alert("Work in Progress.....")
-
+})
+function downloadImage () {
+  alert('Work in Progress.....')
 }
 
 // Event listener for the color selection mode button
 // Event listener for the color selection mode button
-document.getElementById("colorPicker").addEventListener("input", (event) => {
-  currentColor = event.target.value;
-  isColorSelectionMode = true; // Set color selection mode to active
-  isRandomMode = false; // Deactivate random mode
-  isEraserMode = false; // Deactivate eraser mode
-});
+document.getElementById('colorPicker').addEventListener('input', event => {
+  currentColor = event.target.value
+  isColorSelectionMode = true // Set color selection mode to active
+  isRandomMode = false // Deactivate random mode
+  isEraserMode = false // Deactivate eraser mode
+})
 
-let isEraserMode = false; // Flag to check if eraser mode is active
-let isRandomMode = false; // Flag to check if random mode is active
-let isColorSelectionMode = true;
-let showCellBorders = true; // Flag to check if cell borders should be shown
+let isEraserMode = false // Flag to check if eraser mode is active
+let isRandomMode = false // Flag to check if random mode is active
+let isColorSelectionMode = true
+let showCellBorders = true // Flag to check if cell borders should be shown
 
 // Function to toggle cell borders
-function toggleCellBorders() {
-  showCellBorders = !showCellBorders;
-  const pixels = board.getElementsByClassName("pixel");
-  Array.from(pixels).forEach((pixel) => {
-    pixel.classList.toggle("cell-border", showCellBorders);
-  });
+function toggleCellBorders () {
+  showCellBorders = !showCellBorders
+  const pixels = board.getElementsByClassName('pixel')
+  Array.from(pixels).forEach(pixel => {
+    pixel.classList.toggle('cell-border', showCellBorders)
+  })
 }
 
 // Function to clear the canvas
-function clearCanvas() {
-  const pixels = board.getElementsByClassName("pixel");
-  Array.from(pixels).forEach((pixel) => {
-    pixel.style.backgroundColor = "";
-  });
+function clearCanvas () {
+  const pixels = board.getElementsByClassName('pixel')
+  Array.from(pixels).forEach(pixel => {
+    pixel.style.backgroundColor = ''
+  })
 }
 
 // Function to set a random color for each pixel
-function setRandomColorForEachPixel() {
-  isRandomMode = !isRandomMode;
-  const pixels = board.getElementsByClassName("pixel");
-  Array.from(pixels).forEach((pixel) => {
+function setRandomColorForEachPixel () {
+  isRandomMode = !isRandomMode
+  const pixels = board.getElementsByClassName('pixel')
+  Array.from(pixels).forEach(pixel => {
     if (isRandomMode) {
       const randomColor =
-        "#" + Math.floor(Math.random() * 16777215).toString(16);
-      pixel.style.backgroundColor = randomColor;
+        '#' + Math.floor(Math.random() * 16777215).toString(16)
+      pixel.style.backgroundColor = randomColor
     } else {
-      pixel.style.backgroundColor = "";
+      pixel.style.backgroundColor = ''
     }
-  });
+  })
 }
 
 // Event listener for the color wheel mode button
-document.getElementById("colorWheelBtn").addEventListener("click", () => {
-  isColorSelectionMode = true;
-  isRandomMode = false;
-  isEraserMode = false;
-});
+document.getElementById('colorWheelBtn').addEventListener('click', () => {
+  isColorSelectionMode = true
+  isRandomMode = false
+  isEraserMode = false
+})
 
 document
-  .getElementById("toggleGridBtn")
-  .addEventListener("click", toggleCellBorders);
-document
-  .getElementById("clearCanvasBtn")
-  .addEventListener("click", clearCanvas);
+  .getElementById('toggleGridBtn')
+  .addEventListener('click', toggleCellBorders)
+document.getElementById('clearCanvasBtn').addEventListener('click', clearCanvas)
 // Event listener for the random color mode button
-document.getElementById("randomColorBtn").addEventListener("click", () => {
-  isRandomMode = !isRandomMode;
-  isColorSelectionMode = !isRandomMode; // Set color selection mode to active if random mode is off
-  isEraserMode = false; // Deactivate eraser mode
-});
+document.getElementById('randomColorBtn').addEventListener('click', () => {
+  isRandomMode = !isRandomMode
+  isColorSelectionMode = !isRandomMode // Set color selection mode to active if random mode is off
+  isEraserMode = false // Deactivate eraser mode
+})
+
+document.getElementById('selectAnyColorBtn').addEventListener('click', () => {
+  isRandomMode = false
+  isColorSelectionMode = !isRandomMode // Set color selection mode to active if random mode is off
+  isEraserMode = false // Deactivate eraser mode
+  currentColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+  console.log(currentColor)
+  document.getElementById('colorPicker').value = currentColor
+})
 
 // Event listener for the eraser mode button
-document.getElementById("eraserBtn").addEventListener("click", () => {
-  isEraserMode = !isEraserMode;
-  isColorSelectionMode = !isEraserMode; // Set color selection mode to active if eraser mode is off
-  isRandomMode = false; // Deactivate random mode
-});
+document.getElementById('eraserBtn').addEventListener('click', () => {
+  isEraserMode = !isEraserMode
+  isColorSelectionMode = !isEraserMode // Set color selection mode to active if eraser mode is off
+  isRandomMode = false // Deactivate random mode
+})
 
-board.addEventListener("mouseleave", stopDrawing);
+board.addEventListener('mouseleave', stopDrawing)
 
 // Initial creation of pixels with the default grid size
-createPixels(gridSize);
+createPixels(gridSize)
